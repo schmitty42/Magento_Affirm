57d32b146c12e94ba3c61626a254d24fb82755dd
garrett-schlesinger <garrett.schlesinger@affirm.com>
2014-09-15 20:15:41 -0700

    learn more modal + tracking

--- extension/app/code/community/Affirm/Affirm/Block/Payment/Form.php
+++ extension/app/code/community/Affirm/Affirm/Block/Payment/Form.php
@@ -18,14 +18,8 @@ class Affirm_Affirm_Block_Payment_Form extends Mage_Payment_Block_Form
         $html = "<ul class=\"form-list\" id=\"payment_form_affirm\" style=\"display:none;\">";
         $html .= "<li class=\"form-alt\">";
 
-        // heading
-        $html .= "<div style=\"color:#034082; font-size:16px; \">";
-        $html .= "How does Affirm work?";
-        $html .= "</div>";
-
         // sub
         $html .= "<div style=\"color:#6f6f6f; font-size:14px; \">";
-
         $html .= "Just enter your basic information and get approved instantly.";
         $html .= "<br>";
         $html .= "You will complete your payment on the Affirm website";
@@ -40,6 +34,23 @@ class Affirm_Affirm_Block_Payment_Form extends Mage_Payment_Block_Form
         return $html;
     }
 
+    private function getAffirmPublicApiKey()
+    {
+        return Mage::getStoreConfig('payment/affirm/api_key');
+    }
+
+    private function getAffirmJsUrl()
+    {
+        $api_url = Mage::getStoreConfig('payment/affirm/api_url');
+        $domain = parse_url($api_url, PHP_URL_HOST);
+        $domain = str_ireplace('www.', '', $domain);
+        $prefix = 'cdn1.';
+        if (strpos($domain, 'sandbox') === 0) {
+            $prefix = 'cdn1-';
+        }
+        return 'https://' . $prefix . '' . $domain . '/js/v2/affirm.js';
+    }
+
     /* Replaces default label with custom image, conditionally displaying text
      * based on the Affirm product.
      *
@@ -52,12 +63,22 @@ class Affirm_Affirm_Block_Payment_Form extends Mage_Payment_Block_Form
             // TODO(brian): extract html to template
             // TODO(brian): conditionally load based on env config option
             // This is a stopgap until the promo API is ready to go
+            $publicApiKey = $this->getAffirmPublicApiKey();
+            $affirmJsUrl = $this->getAffirmJsUrl();
             $logoSrc = "https://cdn1.affirm.com/images/badges/affirm-logo_78x54.png";
             $html = "<img src=\"" . $logoSrc . "\" width=\"39\" height=\"27\" class=\"v-middle\" />&nbsp;";
-
+            $html .= '<script type="text/javascript" src="' . $affirmJsUrl . '"></script>';
+            $html .= '<script type="text/javascript">'; //src="https://cdn1.affirm-stage.com/js/v2/affirm.js">';
+            $html .= 'function learnMore() {';
+            $html .= 'affirm.setPublicApiKey("' . $publicApiKey . '");';
+            $html .= "var modal = new affirm.ui.modal_widget(affirm.config.learn_more('SplitPay'));";
+            $html .= "modal.on('continue', function(){modal.close()});";
+            $html .= "modal.open();";
+            $html .= '}';
+            $html .= '</script>';
             // TODO(brian): conditionally display based on payment type
             // alt message: $html.= "Buy Now and Pay Later";
-            $html.= "Buy Now with 3 Easy Payments";
+            $html .= 'Buy Now with 3 Easy Payments <a href="javascript:;" onclick="learnMore()">(learn more)</a>';
 
             $this->setMethodLabelAfterHtml($html);
         }
